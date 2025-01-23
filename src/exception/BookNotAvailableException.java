f630e9fadda28e406e913ba9d739a92a88f60961
Azize Karabakla <azize.karabakla@gmail.com>
2025-01-23 02:03:45 +0100

    master

--- src/exception/BookNotAvailableException.java
+++ src/exception/BookNotAvailableException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class BookNotAvailableException extends Exception {
+public class BookNotAvailableException extends RuntimeException {
 
     // Kitap müsait olmadığında fırlatılır.
 
